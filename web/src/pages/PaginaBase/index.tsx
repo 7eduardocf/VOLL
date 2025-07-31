415261842dc173cdc4de27963b7a98b3cd390c56
7eduardocf <eduardocfrota901@gmail.com>
2025-07-30 22:32:20 -0400

    Feito o componente de Atividades e Banner da pagina inicial

---
+++ web/src/pages/PaginaBase/index.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom"
+import Cabecalho from "../../components/Cabecalho"
+import Rodape from "../../components/Rodape"
+
+const PaginaBase = () => {
+  return (
+    <>
+        <Cabecalho/>
+        <main>
+            <Outlet/>
+        </main>
+        <Rodape/>
+    </>
+  )
+}
+
+export default PaginaBase
