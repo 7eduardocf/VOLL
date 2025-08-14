124031192159e78f1f6f36330db42b9faddcc6d1
7eduardocf <eduardocfrota901@gmail.com>
2025-08-13 20:44:09 -0400

    style(Modal): feita a estilização do modal

--- web/src/pages/Dashboard/Modal/index.tsx
+++ web/src/pages/Dashboard/Modal/index.tsx
@@ -1,20 +1,44 @@
 import { Box, Modal } from "@mui/material"
 import Titulo from "../../../components/Titulo"
 import styled from "styled-components"
+import Botao from "../../../components/Botao";
 
 const BoxCustomizado = styled(Box)`
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 30vw;
-    max-height: 90vh;
-    overflow-y: auto;
-    background-color: white;
-    border: none;
-    border-radius: 16px;
-    padding: 1em 5em;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 30vw;
+  max-height: 90vh;
+  overflow-y: auto;
+  background-color: var(--branco);
+  border: none;
+  border-radius: 16px;
+  padding: 1em 5em;
+`;
 
+const Container = styled.div`
+text-align: left;
+`
+
+const ContainerSwitch = styled.div`
+text-align: center;
+`
+
+const TextoSwitch = styled.p`
+color: var(--cinza);
+`
+
+const BotaoCustomizado = styled(Botao)`
+    width: 50%;
+    display: block;
+    margin: 0 auto;
+`
+
+const ContainerEndereco = styled.div`
+display: grid;
+grid-template-columns: 2fr 1fr;
+grid-gap: 0 1em;
 `
 
 function ModalCadastro({ open, handleClose }: { open: boolean, handleClose: () => void }) {
